0fa060ac07980ca045942550a4114beafef5ee06
elvis <elvisptavares@gmail.com>
2022-04-13 16:15:09 -0300

    add repositories

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Repositories\MarcaRepository;
 
 class MarcaController extends Controller
 {
@@ -18,11 +19,27 @@ class MarcaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-      //  $marcas = Marca::all(); // utilizando o metodo de forma estatica
-      $marcas = $this->marca->with('modelos')->get();
-        return response()->json($marcas, 200);
+        $marcaRepository = new MarcaRepository($this->marca);
+        if($request->has('atributos_modelos')){
+            $atributos_modelos = 'modelos:id,' . $request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
+        }else{
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
+        }
+
+        if($request->has('filtro'))
+        {
+            $marcaRepository->filtro($request->filtro);
+        }
+
+        if($request->has('atributos')){
+            $marcaRepository->selectAtributos($request->atributos);
+        }
+
+    
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
 
