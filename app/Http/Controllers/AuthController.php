c9045bddeac2834b1cddea096150d4bea84c334f
elvis <elvisptavares@gmail.com>
2022-05-19 22:36:25 -0300

    autentincando com email e senha

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,9 +6,21 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
+        //autenticação(email senha)
+        $credenciais = $request->all(['email', 'password']);
+        $token = auth('api')->attempt($credenciais);
 
+        if($token){ //usuario autenticado com sucesso
+            return response()->json(['token' => $token]);
+        }else{//erro de usuario ou senha
+            return response()->json(['erro'=> 'usuario ou senha invalido'], 403);
+        }
+
+        //403 - Unauthorized -> nao autorizado
+        //404 - forbidden -> proibido(login inavalido)
+       
     }
 
     public function logout()
